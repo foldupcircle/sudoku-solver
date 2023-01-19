a6532e6d54ccce35cf2a3dfe78c62b6bbedcefcc
Sricharan Guddanti <sriguddanti@gmail.com>
2023-01-18 19:26:16 -0800

    got selected square to work

--- GUI.py
+++ GUI.py
@@ -8,25 +8,36 @@ pygame.display.set_caption('Sudoku Solver')
 BLACK = (0, 0, 0)
 WHITE = (250, 250, 250)
 BLUE = (70, 130, 180)
+RED = (255, 87, 51)
+GRAY = (128,128,128)
 
 FPS = 60
 BUFFER = 3
 THINK_LINE = 10
 FONT_SIZE = 40
 BLOCK_SIZE = int(WIDTH / 9)
-SELECTED = [0,0]
+SELECTED = [0, 0]
 
-def draw_sudoku_grid(bo):
+def draw_sudoku_grid(bo, pos, changed):
     WIN.fill(BLACK)
     for x in range(0, WIDTH, BLOCK_SIZE):
         for y in range(0, HEIGHT, BLOCK_SIZE):
             rectangle = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(WIN, WHITE, rectangle, 1)
-    pygame.draw.lines(WIN, WHITE, True, [(0,0), (0, HEIGHT - BUFFER), (WIDTH - BUFFER, HEIGHT - BUFFER), (WIDTH - BUFFER, 0)], THINK_LINE)
-    pygame.draw.line(WIN, WHITE, (WIDTH / 3,0), (WIDTH / 3,HEIGHT), THINK_LINE)
-    pygame.draw.line(WIN, WHITE, (WIDTH * 2 / 3,0), (WIDTH * 2 / 3,HEIGHT), THINK_LINE)
-    pygame.draw.line(WIN, WHITE, (0,HEIGHT / 3), (WIDTH,HEIGHT / 3), THINK_LINE)
-    pygame.draw.line(WIN, WHITE, (0,HEIGHT * 2 / 3), (WIDTH,HEIGHT * 2 / 3), THINK_LINE)
+    pygame.draw.line(WIN, WHITE, (WIDTH / 3, 0), (WIDTH / 3, HEIGHT), THINK_LINE)
+    pygame.draw.line(WIN, WHITE, (WIDTH * 2 / 3, 0), (WIDTH * 2 / 3, HEIGHT), THINK_LINE)
+    pygame.draw.line(WIN, WHITE, (0, HEIGHT / 3), (WIDTH, HEIGHT / 3), THINK_LINE)
+    pygame.draw.line(WIN, WHITE, (0, HEIGHT * 2 / 3), (WIDTH, HEIGHT * 2 / 3), THINK_LINE)
+
+    if changed:
+        SELECTED[0] = pos[0] // 70
+        SELECTED[1] = pos[1] // 70
+    x = SELECTED[0] * 70
+    y = SELECTED[1] * 70
+    rectangle = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+    WIN.fill(GRAY, rect=rectangle)
+    pygame.draw.rect(WIN, WHITE, rectangle, 1)
+
     font = pygame.font.SysFont('Arial', FONT_SIZE, bold=True)
     for r in range(len(bo)):
         for c in range(len(bo[0])):
@@ -38,74 +49,41 @@ def draw_sudoku_grid(bo):
                 text = font.render(str(bo[r][c]), True, BLUE)
                 rect = text.get_rect(center=(c * 70 + (BLOCK_SIZE / 2), r * 70 + (BLOCK_SIZE / 2)))
                 WIN.blit(text, rect)
+
     pygame.display.update()
 
-def handle_selected(keys_pressed, position):
-    row = SELECTED[0]
-    col = SELECTED[1]
-    if not position:
-        pressed = False
-        if keys_pressed[pygame.K_UP]:
-            rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(WIN, WHITE, rectangle, 1)
-            if SELECTED[0]:
-                SELECTED[0] -= 1
-            pressed = True
-        elif keys_pressed[pygame.K_DOWN]:
-            rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(WIN, WHITE, rectangle, 1)
-            if SELECTED[0] != 8:
-                SELECTED[0] += 1
-            pressed = True
-        elif keys_pressed[pygame.K_RIGHT]:
-            rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(WIN, WHITE, rectangle, 1)
-            if SELECTED[1] != 9:
-                SELECTED[1] += 1
-            pressed = True
-        elif keys_pressed[pygame.K_LEFT]:
-            rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(WIN, WHITE, rectangle, 1)
-            if SELECTED[1]:
-                SELECTED[1] -= 1 
-            pressed = True
-        if pressed:
-            row = SELECTED[0]
-            col = SELECTED[1]
-            rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(WIN, BLUE, rectangle, 10)
-        pygame.display.update()
-    else:
-        rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-        pygame.draw.rect(WIN, WHITE, rectangle, 1)
-        row = position[0] // BLOCK_SIZE
-        col = position[1] // BLOCK_SIZE
-        SELECTED[0] = row
-        SELECTED[1] = col
-        rectangle = pygame.Rect(row * 70, col * 70, BLOCK_SIZE, BLOCK_SIZE)
-        pygame.draw.rect(WIN, BLUE, rectangle, 10)
-    
+def handle_selected(keys_pressed):
+    if keys_pressed[pygame.K_UP] and SELECTED[1] != 0:
+        SELECTED[1] -= 1
+    elif keys_pressed[pygame.K_DOWN] and SELECTED[1] != 8:
+        SELECTED[1] += 1
+    elif keys_pressed[pygame.K_LEFT] and SELECTED[0] != 0:
+        SELECTED[0] -= 1
+    elif keys_pressed[pygame.K_RIGHT] and SELECTED[0] != 8:
+        SELECTED[0] += 1
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     run = True
     bo = original_board
+    pos = 0
+    changed = False
     while run:
         clock.tick(FPS)
-        pos = 0
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-            elif event.type == pygame.MOUSEBUTTONUP:
-                pos = 0
-        draw_sudoku_grid(bo)
-
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_SPACE]:
             bo = solve(board)
-        # handle_selected(keys_pressed, pos)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and pos != pygame.mouse.get_pos():
+                pos = pygame.mouse.get_pos()
+                changed = True
+            else:
+                handle_selected(keys_pressed)
+                changed = False
+        draw_sudoku_grid(bo, pos, changed)
         pygame.display.update()
     pygame.quit()
 
